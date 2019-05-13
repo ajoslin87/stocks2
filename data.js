b17f309a8c67a7a4a1de914cffdbdb97d44a79eb
ajoslin87 <ajoslin87@gmail.com>
2019-05-13 14:00:41 -0400

    file loader works

--- data.js
+++ data.js
@@ -9,16 +9,36 @@ dayTwo = aapl[i+1].close;
 
 dayOverDay = dayTwo-dayOne;
 dayOver.push(dayOverDay);
-console.log(dayOver[i]);
+//console.log(dayOver[i]);
 theMax = (d3.max(dayOver))
+maxIndex = dayOver.indexOf(theMax);
+maxDate = aapl[maxIndex].date;
+
+
   }
+
 
 //console.log(theMax);
 
-maxIndex = dayOver.indexOf(theMax);
-console.log(maxIndex);
+//console.log(maxIndex);
 
-maxDate = aapl[maxIndex].date;
 
-//console.log("On" +" "+ maxDate);
+//console.log("On" +" "+ maxDate + " Apple Changed by " + theMax );
+
+var midiDateScale  = d3.scaleLinear().domain([0,127]).range([0, aapl.length]);
+
+  midiDate = function(){
+  midi = document.getElementById("midi").value
+  midiDateIndex = Math.floor(midiDateScale(midi))
+  document.getElementById("Datehtml").innerHTML = aapl[midiDateIndex].date;
+
+  x.domain([aapl[midiDateIndex].date, dateEnd]);
+
+  svg.select(".x")
+    .transition()
+      .call(xAxis);
+
+}
+
+//console.log(midiDateScale(127))
 };
--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-var aapl = []
-var dayOver = []
-var dot;
+var aapl = [];
+var aaplDates =[];
+var dayOver = [];
 
 var parseDate = d3.timeParse("%Y-%m-%d");
+var prezParse = d3.timeParse("%d/%m/%Y");
+var goldDate = d3.timeParse("%Y-%m");
+
+var adate;
+
+var closestDate;
+
+var prez =[];
+
+myFunc = function(d) {
+
+  dotClose = dateFns.closestTo((parseDate(dot)).getTime(),aaplDates)
+  console.log(dotClose)
+  return d.date.getTime() == dotClose.getTime();
+}
 
 var margin = {
     top: 30,
@@ -26,13 +41,15 @@ var yAxis = d3.axisLeft().scale(y)
   .ticks(5);
 
 // Define the line
-var valueline = d3.line()
-  .x(function(d) {
-    return x(d.date);
-  })
-  .y(function(d) {
-    return y(d.close);
-  })
+var valueline
+
+// var valueline1 = d3.line()
+//   .x(function(d){
+//     return x(d.date);
+//   })
+//   .y(function(d){
+//     return y(d.Adj_Close);
+//   })
 
 // Adds the svg canvas
 var svg = d3.select("body")
@@ -43,103 +60,236 @@ var svg = d3.select("body")
   .attr("transform",
     "translate(" + margin.left + "," + margin.top + ")");
 
-var files = ["AAPL.csv"];
+var files = ["AAPL.csv", "presidents.csv", "gold_monthly.csv"];
+var loadFiles=[];
 var promises = [];
+var files2 =[];
+var promises2 =[];
+
+String.prototype.removeWord = function(searchWord){
+    var str = this;
+    var n = str.search(searchWord);
+    while(str.search(searchWord) > -1){
+        n = str.search(searchWord);
+        str = str.substring(0, n) + str.substring(n + searchTerm.length, str.length);
+    }
+    return str;
+}
+
+
+
+var fileAdded = function(){
+
+  file = document.getElementById("avatar").files[0];
+  //d3fileObject = d3.csv(file.name)
+
+
+  //files2.push(file);
+  console.log(file);
+  //console.log(files2);
+
+
+
+
+      // function readIt(data){
+        d3.csv(file.name).then(function(result){
+           console.log(result)
+    })
+
+}
+
+
+
 
 files.forEach(function(url) {
   promises.push(d3.csv(url))
 });
 
+
+
 Promise.all(promises).then(function(data) {
-  //d3.csv("AAPL.csv").then(function(data) {
-  console.log("data", data);
+  //console.log("data", data[1]);
   data[0].forEach(function(d) {
     d.date = parseDate(d.date);
-    //console.log(d.date);
     d.close = +d.close;
-    //console.log(d.close)
   });
+ console.log
+  data[1].forEach(function(d){
+    d.Took_office = prezParse(d.Took_office)
+    d.Left_office = prezParse(d.Left_office)
+  })
+
+  data[2].forEach(function(d){
+    d.Date = goldDate(d.Date)
+    d.Price = +d.Price;
+  })
 
   columns = data[0].columns;
 
-  console.log(columns);
+  console.log(data[1]);
+
+  for (i = 0; i < data[0].length; i++) {
+
+    aapl.push(data[0][i]);
+    aaplDates.push(aapl[i].date)
+  };
+  //console.log(aapl);
+
+  for(i=0; i<data[1].length; i++ ){
+    prez.push(data[1][i].President)
+  };
+
+
 
 });
 
 Promise.all(promises).then(function(data) {
 
-  for (i = 0; i < data.length; i++) {
-    aapl.push(data[i]);
-  };
+var prezi = document.getElementById("prezi");
 
+  for(var i=0; i<data[1].length; i++){
+    var option = document.createElement("OPTION"),
+    txt=document.createTextNode(prez[i]);
+    option.appendChild(txt);
+    option.setAttribute("value",prez[i]);
+    prezi.insertBefore(option,prezi.lastChild);
+  }
+
+var lini = document.getElementById("lini");
+
+  for(var i=0; i<columns.length; i++){
+    var option = document.createElement("OPTION"),
+    txt=document.createTextNode(columns[i]);
+    option.appendChild(txt);
+    option.setAttribute("value",columns[i]);
+    lini.insertBefore(option,lini.lastChild);
+
+  }
+});
+
+
+  Promise.all(promises).then(function(data) {
 
   // Scale the range of the data
 
   //x.domain([parseDate('2008-10-27'), parseDate('2009-02-16')]);
-  x.domain(d3.extent(data[0], function(d) {
-    return d.date;
-  }));
 
-  y.domain([0, d3.max(data[0], function(d) {
-    return d.close;
-  })]);
+
+
   //y.domain([0, 50]);
 
+});
+Promise.all(promises).then(function(data) {
+
+var i = 0;
+
+addLine = function(){
   // Add the valueline path.
-  svg.append("path")
-    .attr("class", "line")
-    .attr("d", valueline(data[0]))
-    .style("fill", "none")
-    .style("stroke", "#000");
 
-  myFunc = function(d) {
-    return (d.date.getTime() == (parseDate('2014-07-07')).getTime())
-  }
+theVal = lini.value
+console.log(theVal);
 
-  // Add the scatterplot
+y.domain([0, d3.max(data[0], function(d) {
+  return d[theVal];
+})]);
 
-  //addDot = function() {
 
-    dot = document.getElementById("dot").value;
+dateStart =parseDate(document.getElementById("dateStart").value);
+dateEnd = parseDate(document.getElementById("dateEnd").value);
 
-    svg.selectAll("dot")
-      .data(data[0])
-      .enter().append("circle")
-      .filter(myFunc)
-      //.filter(function(d) { return d.date.getTime() == parseDate('2009-01-05').getTime})
-      .style("fill", "red")
-      .attr("r", 3.5)
-      .attr("cx", function(d) {
-        return x(d.date);
-      })
-      .attr("cy", function(d) {
-        return y(d.close);
-      })
 
-//}
-svg.selectAll("rect")
-  .data(data[0])
-  .enter().append('rect')
-  .filter(myFunc)
-  .style("opacity", .5)
-  .style("fill", "red")
-  .attr("x", function(d){
+if(dateStart==null){
+  dateStart = (data[0][0].date)
+ //console.log(dateStart)
+}
+
+if(dateEnd ==null){
+  dateEnd =((data[0][data[0].length-1]).date)
+}
+
+x.domain([dateStart, dateEnd]);
+
+valueline = d3.line()
+  .x(function(d) {
     return x(d.date);
   })
-  .attr("y", 0)
-  .attr("width",  100)
-  .attr("height", height);
+  .y(function(d) {
+    return y(d[theVal]);
+  })
 
 
-  // Add the X Axis
-  svg.append("g")
-    .attr("class", "x axis")
-    .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
-
-  // Add the Y Axis
+if(i==0){
   svg.append("g")
     .attr("class", "y axis")
+    .transition(6000)
     .call(yAxis);
+
+    svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")").transition()
+
+      .call(xAxis);
+
+}
+
+  var  lines = svg.selectAll("path")
+  .data(data[0]).enter().append("path").transition().duration(7000)
+    .attr("class", "line")
+    .attr("d", valueline(data[0]))
+
+    .style("fill", "none")
+    .style("stroke", "#000")
+    ;
+
+  i++
+};
+
+
+changeDateRange = function(){
+
+  dateStart = parseDate(document.getElementById("dateStart").value)
+  dateEnd = parseDate(document.getElementById("dateEnd").value)
+
+console.log("date Start ",dateStart);
+console.log("date end" ,dateEnd);
+
+  if(dateStart==null){
+    dateStart = (data[0][0]).date
+   console.log("date Start", dateStart)
+  console.log("fuck your Haircut")
+  }
+
+  if(dateEnd ==null){
+    dateEnd =(data[0][data[0].length-1]).date
+    console.log("date End", dateEnd)
+  }
+
+  var svg = d3.select("body").transition();
+
+
+console.log("date End", dateEnd)
+console.log("date Start", dateStart)
+
+   x.domain([dateStart, dateEnd]);
+
+  svg.select(".line")
+  .attr("d", valueline(data[0]))
+  svg.select(".x")
+    .transition()
+      .call(xAxis)
+
+      ;
+
+
+}
+
+update =  function() {
+  			x.domain([0, Math.random(10, 10000)]);
+
+  			svg.select(".x")
+  				.transition()
+  					.call(xAxis);
+
+  		}
 
 });
